e9016198e48ca8cc257543330eed4205a25e49a8
Keith Fiske <keith@keithf4.com>
2023-08-16 11:49:11 -0400

    Update extension to version 1.6

--- pgnodemx--1.6.sql
+++ pgnodemx--1.6.sql
@@ -1,4 +1,4 @@
-/* pgnodemx--1.5.sql */
+/* pgnodemx--1.6.sql */
 
 -- complain if script is sourced in psql, rather than via CREATE EXTENSION
 \echo Use "CREATE EXTENSION pgnodemx" to load this file. \quit
